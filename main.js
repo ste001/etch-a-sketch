965ab907e82933c38e500c1f32f80eb796a9d003
Stefano Merazzi <asso99@hotmail.com>
2018-11-03 15:57:14 +0100

    Add trail on Grid items, change Grid separators color to gray

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-createGrid(4);
-const item = document.getElementsByClassName("item);
-item.addEventListener("mouseenter", changeItemColor);
-
-
+createGrid(10);
+initEventListeners();
 
 function createGrid(squares){
 	const grid = document.querySelector('.grid-container');
+	const gridStyle = grid.style;
+	gridStyle.gridTemplateColumns = "repeat(" + squares + ", auto)";
+	gridStyle.gridTemplateRows = "repeat(" + squares + ", auto)";
 	let total = squares*squares;
 	for (let i = 0; i < total; i++){
 		const div = document.createElement('div');
@@ -14,5 +14,14 @@ function createGrid(squares){
 	}
 }
 
-function changeItemColor(){
+function changeItemColor(e){
+	const style = e.target.style;
+	style.backgroundColor = "black";
+}
+
+function initEventListeners(){
+	const items = document.querySelectorAll(".item");
+	items.forEach((item)=>{
+		item.addEventListener('mouseenter', changeItemColor);
+	});
 }
